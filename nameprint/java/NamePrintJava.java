6669666de60220d564e5b28ceabfeceb4a9ab4a4
DJRico <dale.rico@usm.edu>
2017-11-09 14:46:36 -0600

    Add files via upload

---
+++ nameprint/java/NamePrintJava.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nameprint.java;
+
+/**
+ *
+ * @author dalerico
+ */
+public class NamePrintJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println();
+        
+    System.out.println(" wwwwww	        wwwwwwwww   	ww          wwwwwwwww ");
+    System.out.println(" ww    ww    	ww     ww   	ww          ww ");       
+    System.out.println(" ww      ww   	ww     ww  	ww          ww ");
+    System.out.println(" ww   	 ww	wwwwwwwww	ww          wwwwwwwww ");
+    System.out.println(" ww      ww	ww     ww	ww          ww ");
+    System.out.println(" ww    ww	ww     ww	ww          ww ");
+    System.out.println(" wwwwww		ww     ww	wwwwwwwww   wwwwwwwww ");
+    
+    System.out.println("*******************************************************");
+    
+   System.out.println("  ///////////");
+   System.out.println(" +ooooooooooo+ ");
+   System.out.println("  |         | ");
+   System.out.println("  |  ^    ^ | ");
+   System.out.println(" C|     u   | ");
+   System.out.println("  |  (  w  )| ");
+   System.out.println("  |_________| ");
+        
+    }
+    
+}
